4d3d75d0df2eac76fbd894b22d99c9cd5c91bdac
Henry Tirla <Henrytirla@gmail.com>
2020-04-10 04:05:08 +0200

    Question 5,6,7 chapter 2

---
+++ Chapter two Exercise/Q_7.py
@@ -0,0 +1,18 @@
+"""Write a program that uses exactly four for loops to print the sequence of letters below.
+AAAAAAAAAABBBBBBBCDCDCDCDEFFFFFFG"""
+
+for i in range(10):
+    print("A",end="")
+for i in range(7):
+    print("B", end="")
+for i in range(4):
+    print("CD",end="")
+print("E", end="") #Take not of this it's not in the for loop Prints only once only E and G
+
+for i in range(5):
+        print("F",end="")
+print("G", end="")
+
+
+
+
