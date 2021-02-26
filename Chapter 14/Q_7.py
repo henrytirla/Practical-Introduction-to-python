6609eef5c24f7cfbce2000eebdaeede39aea782b
Henry Tirla <Henrytirla@gmail.com>
2021-02-26 22:44:57 +0100

    Q_7

---
+++ Chapter 14/Q_7.py
@@ -0,0 +1,9 @@
+"""Use the Standard_deck class of this section to create a simplified version of the game War.
+In this game, there are two players. Each starts with half of a deck. The players each deal
+the top card from their decks and whoever has the higher card wins the other player’s cards
+and adds them to the bottom of his deck. If there is a tie, the two cards are eliminated from
+play (this differs from the actual game, but is simpler to program). The game ends when one
+player runs out of cards."""
+
+class Standard_deck():
+    pass
