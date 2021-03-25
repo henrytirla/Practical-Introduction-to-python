660bf0953ffa5dc002dc520f861feec251c427d2
Henry Tirla <Henrytirla@gmail.com>
2021-03-25 01:13:32 +0100

    Chapter 9

---
+++ Chapter 9 Exercise/Q_1.py
@@ -0,0 +1,8 @@
+"""The code below prints the numbers from 1 to 50. Rewrite the code using a while loop to
+accomplish the same thing.
+for i in range(1,51):
+print(i)"""
+num =0
+while num < 51:
+    print(num)
+    num+=1
