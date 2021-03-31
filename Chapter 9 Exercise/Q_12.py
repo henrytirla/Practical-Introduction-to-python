9388c00381fc2d9ea6efde3a8b1bca1b18985264
Henry Tirla <Henrytirla@gmail.com>
2021-03-31 22:55:51 +0200

    Q_12

---
+++ Chapter 9 Exercise/Q_12.py
@@ -0,0 +1,3 @@
+"""Write a program in which you have a list that contains seven integers that can be 0 or 1. Find
+the first nonzero entry in the list and change it to a 1. If there are no nonzero entries, print a
+message saying so."""
