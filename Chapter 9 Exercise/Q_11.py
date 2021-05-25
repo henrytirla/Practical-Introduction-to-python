c97a8eeb62e4faae35a26019e4730b6368df7198
Henry Tirla <Henrytirla@gmail.com>
2021-05-26 01:02:34 +0200

    Solved

--- Chapter 9 Exercise/Q_11.py
+++ Chapter 9 Exercise/Q_11.py
@@ -1,2 +1,20 @@
-"""Write a program that starts with an 5  5 list of zeroes and randomly changes exactly ten of
+"""Write a program that starts with an 5 X 5 list of zeroes and randomly changes exactly ten of
 those zeroes to ones."""
+from random import randint
+list_50 =[0,0,0,0,0,
+          0,0,0,0,0,
+          0,0,0,0,0,
+          0,0,0,0,0,
+          0,0,0,0,0,]
+i=0
+total_zeros = len(list_50)
+while (i <1):
+    value = randint(0, total_zeros-1)
+    list_50[value] = 1
+    i = i+1
+print(list_50)
+
+
+
+
+
