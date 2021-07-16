f9eb0cc7a1fde9cce3ac3b757df7b054a69c10c1
henrytirla <henrytirla@gmail.com>
2021-07-16 06:44:18 +0200

    Merge without sort done

--- Chapter 13 Exercise/Q_20.py
+++ Chapter 13 Exercise/Q_20.py
@@ -12,8 +12,17 @@ def merge_withsort(l1,l2):
 merge_withsort([1,3,4,6],[2,4,5,6,7])
 
 
-def merge_withoutsort():
-    pass
+def merge_withoutsort(l1,l2):
+    l3 =l1+l2
+
+    for i in range(len(l3)):
+        for j in range(1+i,len(l3)):
+            if l3[i] > l3[j]:
+                l3[i], l3[j] = l3[j], l3[i]
+
+    print(l3)
+
+merge_withoutsort([2,5,4,6,1],[7,6,5,4,3,0])
 
 
     
