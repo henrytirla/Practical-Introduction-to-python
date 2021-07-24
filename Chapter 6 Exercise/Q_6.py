e22a6b90419db178a4ffdeeabb70f8c840e2eb43
henrytirla <henrytirla@gmail.com>
2021-07-24 12:38:50 +0200

    Q6

--- Chapter 6 Exercise/Q_6.py
+++ Chapter 6 Exercise/Q_6.py
@@ -1,2 +1,12 @@
 
-## TODO
+"""Write a program that asks the user to enter a string s and then converts s to lowercase, removes
+all the periods and commas from s, and prints the resulting string."""
+
+
+input_string= input("Please Enter a String: ")
+input_string = input_string.lower()
+input_string = input_string.replace('.','')
+input_string = input_string.replace(',','')
+print(input_string)
+
+# Example This , is a beautiful , day.
