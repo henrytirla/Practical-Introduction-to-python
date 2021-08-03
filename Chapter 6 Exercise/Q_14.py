191a47cb455aae9877cc07408b5788b8dd38d558
henrytirla <henrytirla@gmail.com>
2021-08-03 14:41:53 +0200

    Q_14

---
+++ Chapter 6 Exercise/Q_14.py
@@ -0,0 +1,6 @@
+"""Write a program that asks the user to enter their name in lowercase and then capitalizes the
+first letter of each word of their name."""
+
+user_input = input("Enter Your names in lower case :")
+
+print(user_input.title())
