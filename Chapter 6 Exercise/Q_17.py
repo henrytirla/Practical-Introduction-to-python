31776a2391bb4ca05f0a4d3ee24a0c1824399fc1
henrytirla <henrytirla@gmail.com>
2021-09-09 23:41:00 +0100

    Q_17

--- Chapter 6 Exercise/Q_17.py
+++ Chapter 6 Exercise/Q_17.py
@@ -8,7 +8,11 @@ cdefghijklmnopqrstuvwxyzab
 yzabcdefghijklmnopqrstuvwx
 zabcdefghijklmnopqrstuvwxy
 """
-list_alphabet = ["a", "b","c","d","e","f","g","h","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+list_alphabet = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+j=0
+for i in range(len(list_alphabet)):
+    print("---",list_alphabet[i], end="")
+    for j in range(i + 1, len(list_alphabet)):
+        print(list_alphabet[j],end="")
 
-for i in  range(len(list_alphabet)):
-    print(list_alphabet[i],end="")
+##Not complete solution
