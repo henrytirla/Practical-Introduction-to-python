b1fe2afd3c478f15769d65267937edcabce9a2dc
henrytirla <henrytirla@gmail.com>
2021-09-10 01:08:50 +0100

    Q_18

---
+++ Chapter 6 Exercise/Q_18.py
@@ -0,0 +1,15 @@
+"""
+
+The goal of this exercise is to see if you can mimic the behavior of the in operator and the
+count and index methods using only variables, for loops, and if statements.
+(a) Without using the in operator, write a program that asks the user for a string and a letter
+and prints out whether or not the letter appears in the string.
+
+(b) Without using the count method, write a program that asks the user for a string and a
+letter and counts how many occurrences there are of the letter in the string.
+
+(c) Without using the index method, write a program that asks the user for a string and
+a letter and prints out the index of the first occurrence of the letter in the string. If the
+letter is not in the string, the program should say so
+"""
+
