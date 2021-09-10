c2ebc3c5543a2ab0ac17f0b662960e6ffa9a6a5f
henrytirla <henrytirla@gmail.com>
2021-09-10 01:03:21 +0100

    Q_17

--- Chapter 6 Exercise/Q_17.py
+++ Chapter 6 Exercise/Q_17.py
@@ -8,11 +8,11 @@ cdefghijklmnopqrstuvwxyzab
 yzabcdefghijklmnopqrstuvwx
 zabcdefghijklmnopqrstuvwxy
 """
-list_alphabet = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-for i in range(len(list_alphabet)):
-    print("---",list_alphabet[i], end="")
-    for j in range(i + 1, len(list_alphabet)):
-        print(list_alphabet[j],end="")
+alphabet="abcdefghijklmnopqrstuvwxyz"
 
-##Not complete solution
+for m in range (26):
+    for j in range (26):
+        print(alphabet[(m+j)%26],end='')
+    print()
+
