27ff494ceebe4c29bf501a08ebf81058d18a9edc
henrytirla <henrytirla@gmail.com>
2021-09-28 01:58:11 +0100

    Q1

--- Chapter 8 Exercise/Q_1.py
+++ Chapter 8 Exercise/Q_1.py
@@ -1,2 +1,12 @@
 """Write a program that asks the user to enter some text and then counts how many articles are
 in the text. Articles are the words 'a', 'an', and 'the'."""
+
+user_input =input("Enter Text: ") #love is a beautiful and profound an angelic being
+articles =["a","an","and"]
+words =user_input.split()
+count =0
+
+for word in words:
+    if word in articles:
+        count +=1
+print(count)
