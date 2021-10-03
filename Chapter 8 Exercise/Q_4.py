bd1710b7293de11448987547d2334c9cc23b9e19
henrytirla <henrytirla@gmail.com>
2021-10-04 00:29:36 +0100

    Q4

---
+++ Chapter 8 Exercise/Q_4.py
@@ -0,0 +1,6 @@
+"""4. (a) Write a program that asks the user to enter a sentence and then randomly rearranges the
+words of the sentence. Don’t worry about getting punctuation or capitalization correct.
+
+(b) Do the above problem, but now make sure that the sentence starts with a capital, that
+the original first word is not capitalized if it comes in the middle of the sentence, and
+that the period is in the right place."""
