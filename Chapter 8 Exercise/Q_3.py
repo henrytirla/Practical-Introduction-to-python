ac48ae4bea335472c1830b3433f4fa700befeb02
henrytirla <henrytirla@gmail.com>
2021-10-04 00:25:47 +0100

    Q3

--- Chapter 8 Exercise/Q_3.py
+++ Chapter 8 Exercise/Q_3.py
@@ -1,2 +1,11 @@
 """(a) Ask the user to enter a sentence and print out the third word of the sentence.
 (b) Ask the user to enter a sentence and print out every third word of the sentence."""
+
+user_input = input("Enter a sentence: ")
+#eg I love you ethan tirla and I promise to give you the best life has to offer
+sentence_words = user_input.split()
+
+print("a",sentence_words[2])
+
+for word in range(2,len(sentence_words),3):
+    print("b",sentence_words[word])
