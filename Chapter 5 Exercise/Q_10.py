f536f8e2e914f9f5801c81c90501ee10d44e11e8
wlacey <42483621+wlacey@users.noreply.github.com>
2021-10-14 11:50:30 +0900

    Update Q_10.py

--- Chapter 5 Exercise/Q_10.py
+++ Chapter 5 Exercise/Q_10.py
@@ -1,3 +1,13 @@
+"""
+Ask the user to enter 10 test scores. Write a program to do the following:
+(a) Print out the highest and lowest scores.
+(b) Print out the average of the scores.
+(c) Print out the second largest score.
+(d) If any of the scores is greater than 100, then after all the scores have been entered, print
+a message warning the user that a value over 100 has been entered.
+(e) Drop the two lowest scores and print out the average of the rest of them.
+"""
+
 from statistics import mean
 
 mylist = []
