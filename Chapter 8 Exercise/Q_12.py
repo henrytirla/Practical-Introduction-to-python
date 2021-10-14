08a1f6f3eddf1c2d40063fc32b97ac12bc0f9964
wlacey <42483621+wlacey@users.noreply.github.com>
2021-10-14 11:55:54 +0900

    Update Q_12.py

--- Chapter 8 Exercise/Q_12.py
+++ Chapter 8 Exercise/Q_12.py
@@ -1,3 +1,11 @@
+"""
+Write a program that gets a string from the user containing a potential telephone number.
+The program should print Valid if it decides the phone number is a real phone number, and
+Invalid otherwise. A phone number is considered valid as long as it is written in the form
+abc-def-hijk or 1-abc-def-hijk. The dashes must be included, the phone number should contain
+only numbers and dashes, and the number of digits in each group must be correct.
+"""
+
 def validate_input(message):
     valid = False
     while not valid:
