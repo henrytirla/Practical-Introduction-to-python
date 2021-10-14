f6bb9aa77c598d1f345bdab59ec0e55c353b09ec
wlacey <42483621+wlacey@users.noreply.github.com>
2021-10-14 11:49:10 +0900

    Update Q_12.py

--- Chapter 5 Exercise/Q_12.py
+++ Chapter 5 Exercise/Q_12.py
@@ -1,3 +1,9 @@
+"""
+Write a program that asks the user to guess a random number between 1 and 10. If they guess
+right, they get 10 points added to their score, and they lose 1 point for an incorrect guess. Give
+the user five numbers to guess and print their score after all the guessing is done.
+"""
+
 from random import randint
 
 score = 0
