88314ef6fda4afdc65179ff04a2e256ce8fbd754
wlacey <42483621+wlacey@users.noreply.github.com>
2021-10-14 12:01:41 +0900

    Update Q_19.py

--- Chapter 8 Exercise/Q_19.py
+++ Chapter 8 Exercise/Q_19.py
@@ -1,4 +1,10 @@
+"""
+Write a program that creates and prints an 8 × 8 list whose entries alternate between 1 and 2
+in a checkerboard pattern, starting with 1 in the upper left corner.
+"""
+
 print('\nProblem 19\n')
+
 from pprint import pprint
 from itertools import cycle
 
@@ -6,7 +12,10 @@ iterator = cycle(range(2))
 L = [[next(iterator) for column in range(8)] for row in range(8)]
 pprint(L)
 
+
+
 print('\nProblem 19 - Alternate Solution\n')
+
 from pprint import pprint
 
 def alternate():
