e80ecc67e65491500d02ede51b00b8d2eda0179e
wlacey <42483621+wlacey@users.noreply.github.com>
2021-10-14 11:56:38 +0900

    Update Q_13.py

--- Chapter 8 Exercise/Q_13.py
+++ Chapter 8 Exercise/Q_13.py
@@ -1,3 +1,11 @@
+"""
+Let L be a list of strings. Write list comprehensions that create new lists from L for each of the
+following.
+(a) A list that consists of the strings of s with their first characters removed
+(b) A list of the lengths of the strings of s
+(c) A list that consists of only those strings of s that are at least three characters long
+"""
+
 L = ['apple', 'car', 'tree', 'I', 'watch', 'internet']
 print(L)
 M = [s[1:] for s in L]
