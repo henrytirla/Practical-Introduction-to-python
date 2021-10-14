052d31c19ac125474f1f1e24f88b14c78fd98e4a
wlacey <42483621+wlacey@users.noreply.github.com>
2021-10-14 11:45:43 +0900

    Update Q_14.py

--- Chapter 5 Exercise/Q_14.py
+++ Chapter 5 Exercise/Q_14.py
@@ -1,3 +1,13 @@
+"""
+This exercise is about the well-known Monty Hall problem. In the problem, you are a contestant on a game show. The host, Monty Hall, shows you three doors. 
+Behind one of those doors is a prize, and behind the other two doors are goats. You pick a door. Monty Hall, who knows behind which door the prize lies, 
+then opens up one of the doors that doesn’t contain the prize. There are now two doors left, and Monty gives you the opportunity to change your
+choice. Should you keep the same door, change doors, or does it not matter?
+
+Write a program that simulates playing this game 10000 times and calculates what percentage of the time you would win if you switch and what percentage of the time you
+would win by not switching.
+"""
+
 from random import randint, choice
 
 count = 0
