de8441bbf6d1539e28c891ba046e2c2588fd7bd3
henrytirla <henrytirla@gmail.com>
2021-12-14 18:10:33 +0100

    finishing up chapter 8

--- Chapter 8 Exercise/Q_4.py
+++ Chapter 8 Exercise/Q_4.py
@@ -1,12 +1,16 @@
-"""4. (a) Write a program that asks the user to enter a sentence and then randomly rearranges the
-words of the sentence. Don’t worry about getting punctuation or capitalization correct.
+"""
 
-(b) Do the above problem, but now make sure that the sentence starts with a capital, that
+Write a program that asks the user to enter a sentence and
+ then randomly rearranges the words of the sentence.
+ Don’t worry about getting punctuation or capitalization correct.
+
+ b) Do the above problem, but now make sure that the sentence starts with a capital, that
 the original first word is not capitalized if it comes in the middle of the sentence, and
-that the period is in the right place."""
-import random
+that the period is in the right place.
+"""
+
 from random import shuffle
-user_input = input("Enter a sentence: ")
-words = user_input.split()
-word=random.shuffle(words)
-print(words)
+user_sentence = input("Enter a sentence:  ")
+L =user_sentence.split()
+shuffle(L)
+print(L)
