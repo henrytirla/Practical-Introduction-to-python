ae0df556a2cb331dbbdf6095d44e7af710a4d616
Henry Tirla <henrytirla@gmail.com>
2023-04-17 13:03:04 +0100

    Q_18

--- Chapter 6 Exercise/Q_18.py
+++ Chapter 6 Exercise/Q_18.py
@@ -2,6 +2,7 @@
 
 The goal of this exercise is to see if you can mimic the behavior of the in operator and the
 count and index methods using only variables, for loops, and if statements.
+
 (a) Without using the in operator, write a program that asks the user for a string and a letter
 and prints out whether or not the letter appears in the string.
 
@@ -15,40 +16,44 @@ letter is not in the string, the program should say so
 
 
 #a
-
-# string_input = input("Enter String: ")
-# letter_input = input("Enter Character: ")
+# string = input("Enter String: ")
+# letter = input("Enter Letter: ")
+# index =0
+# found= False
 #
 #
-# index = 0
-#
-# while index < len(string_input):
-#     if string_input[index] == letter_input:
-#         print(True)
+# while index < len(string):
+#     if string[index] == letter:
+#         found= True
+#         #print("Letter Found")
 #         break
 #     else:
 #         index+=1
-# print("No match found")
+
+
+
+
+
+
 
 #b
 
-string_input = input("Enter String: ")
-letter_input = input("Enter Character: ")
+string = input("Enter String: ")
+letter = input("Enter Letter: ")
+index =0
+found= False
+count= 0
 
 
-index = 0
-count = 0
-while index < len(string_input):
-    if string_input[index] == letter_input:
-        print(True)
-        index += 1
-        count+=1
+while index < len(string):
+
+    if string[index] == letter:
+        index+=1
+        count +=1
 
 
     else:
-        index+=1
+        index += 1
 print(f"Number occures {count} number of times")
-
-
 
 #c edit solution a to have answer to c
